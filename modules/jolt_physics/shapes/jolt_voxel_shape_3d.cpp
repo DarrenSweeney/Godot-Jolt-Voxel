9c675fbdaac6becdd0252f8c328183cb58053c8e
Voxel <darrensweeneydev@gmail.com>
2026-03-05 22:48:25 +0100

    linux compiler unused var

--- modules/jolt_physics/shapes/jolt_voxel_shape_3d.cpp
+++ modules/jolt_physics/shapes/jolt_voxel_shape_3d.cpp
@@ -10,7 +10,6 @@
 JPH::ShapeRefC JoltVoxelShape3D::_build() const
 {
 	const float min_half_extent = (float)size[size.min_axis_index()];
-	const float actual_margin = MIN(margin, min_half_extent * JoltProjectSettings::collision_margin_fraction);
 
 	VoxelShapeSettings shape_settings;
 	shape_settings.half_extents = to_jolt(size);
